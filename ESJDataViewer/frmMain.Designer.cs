4a749793aabdfc3c43d68c09c09e257b1b0bed9b
lruiz <ruizluisr@gmail.com>
2023-08-18 23:09:07 -0400

    Added Project ESJDataviewer

---
+++ ESJDataViewer/frmMain.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace ESJDataViewer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblInterval = new System.Windows.Forms.Label();
+            this.lblOwnerNumber = new System.Windows.Forms.Label();
+            this.txtOwnerNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtButton = new System.Windows.Forms.Button();
+            this.txtInterval = new System.Windows.Forms.TextBox();
+            this.cbInterval = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 80);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1008, 596);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cbInterval);
+            this.panel1.Controls.Add(this.txtButton);
+            this.panel1.Controls.Add(this.lblInterval);
+            this.panel1.Controls.Add(this.txtInterval);
+            this.panel1.Controls.Add(this.lblOwnerNumber);
+            this.panel1.Controls.Add(this.txtOwnerNumber);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1008, 80);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblInterval
+            // 
+            this.lblInterval.Location = new System.Drawing.Point(586, 12);
+            this.lblInterval.Name = "lblInterval";
+            this.lblInterval.Size = new System.Drawing.Size(84, 23);
+            this.lblInterval.TabIndex = 6;
+            this.lblInterval.Text = "Interval #";
+            this.lblInterval.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblOwnerNumber
+            // 
+            this.lblOwnerNumber.Location = new System.Drawing.Point(295, 11);
+            this.lblOwnerNumber.Name = "lblOwnerNumber";
+            this.lblOwnerNumber.Size = new System.Drawing.Size(84, 23);
+            this.lblOwnerNumber.TabIndex = 4;
+            this.lblOwnerNumber.Text = "Owner Number";
+            this.lblOwnerNumber.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtOwnerNumber
+            // 
+            this.txtOwnerNumber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtOwnerNumber.Location = new System.Drawing.Point(385, 13);
+            this.txtOwnerNumber.Name = "txtOwnerNumber";
+            this.txtOwnerNumber.Size = new System.Drawing.Size(192, 20);
+            this.txtOwnerNumber.TabIndex = 3;
+            this.txtOwnerNumber.TextChanged += new System.EventHandler(this.textbox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(4, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtName
+            // 
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtName.Location = new System.Drawing.Point(94, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(192, 20);
+            this.txtName.TabIndex = 1;
+            this.txtName.TextChanged += new System.EventHandler(this.textbox_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(887, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(121, 80);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 676);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1008, 53);
+            this.panel2.TabIndex = 2;
+            // 
+            // txtButton
+            // 
+            this.txtButton.Location = new System.Drawing.Point(7, 49);
+            this.txtButton.Name = "txtButton";
+            this.txtButton.Size = new System.Drawing.Size(65, 25);
+            this.txtButton.TabIndex = 7;
+            this.txtButton.Text = "Clear Filter";
+            this.txtButton.UseVisualStyleBackColor = true;
+            this.txtButton.Click += new System.EventHandler(this.txtButton_Click);
+            // 
+            // txtInterval
+            // 
+            this.txtInterval.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtInterval.Location = new System.Drawing.Point(676, 41);
+            this.txtInterval.Name = "txtInterval";
+            this.txtInterval.Size = new System.Drawing.Size(192, 20);
+            this.txtInterval.TabIndex = 5;
+            this.txtInterval.Visible = false;
+            this.txtInterval.TextChanged += new System.EventHandler(this.textbox_TextChanged);
+            // 
+            // cbInterval
+            // 
+            this.cbInterval.FormattingEnabled = true;
+            this.cbInterval.Location = new System.Drawing.Point(676, 14);
+            this.cbInterval.Name = "cbInterval";
+            this.cbInterval.Size = new System.Drawing.Size(192, 21);
+            this.cbInterval.TabIndex = 8;
+            this.cbInterval.SelectedIndexChanged += new System.EventHandler(this.cbInterval_SelectedIndexChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblInterval;
+        private System.Windows.Forms.Label lblOwnerNumber;
+        private System.Windows.Forms.TextBox txtOwnerNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button txtButton;
+        private System.Windows.Forms.TextBox txtInterval;
+        private System.Windows.Forms.ComboBox cbInterval;
+    }
+}
+
---
+++ ESJDataViewer/frmMain.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CsvHelper;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ESJDataViewer
+{
+    public partial class frmMain : Form
+    {
+        List<VOListing> Sourcerecords;
+        List<VOListing> filteredlist;
+        public frmMain()
+        {
+            InitializeComponent();
+            Getdata();
+        }
+
+        private DataTable Getdata()
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("VO NAME");
+            dt.Columns.Add("OwnerNumber");
+            dt.Columns.Add("UnitNumber");
+            dt.Columns.Add("IntervalNumber");
+            dt.Columns.Add("Address1");
+            dt.Columns.Add("Address2");
+            dt.Columns.Add("City");
+            dt.Columns.Add("State");
+            dt.Columns.Add("Zip");
+            dt.Columns.Add("Country");
+            dt.Columns.Add("Phone1");
+            dt.Columns.Add("Phone2");
+            dt.Columns.Add("Email");
+            dt.Columns.Add("CUorVO");
+            dt.Columns.Add("EF or 1B");            
+            dt.Columns.Add("BalanceOwing");
+
+
+            var reader = new StreamReader("VO LISTING INCLUDING BALANCE OWED_not including 2023_2024 fees.csv");
+            var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+            var records = csv.GetRecords<VOListing>();
+
+            Sourcerecords = records.ToList();
+
+            dataGridView1.DataSource = Sourcerecords;
+            int value = 0;
+            List<string> Interval = (from val in Sourcerecords
+                                     orderby Int32.Parse(val.IntervalNumber.Trim() == ""?"0":val.IntervalNumber.Trim())
+                                     //orderby val.IntervalNumber 
+                                     select val.IntervalNumber).Distinct().ToList();
+            cbInterval.DataSource = Interval;
+
+            foreach (var r in records)
+            { 
+            
+            }
+
+            return dt;
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            TextBox txt = sender as TextBox;
+
+            
+            if (txt.Name == txtName.Name)
+            {
+                if (txt.Text == "")
+                {
+                    dataGridView1.DataSource = Sourcerecords;
+                }
+                else
+                {
+                    //List<VOListing> filteredlist;
+
+                    //if (txtInterval.Text != "" || txtOwnerNumber.Text != "")
+                    //{filteredlist = filteredlist.FindAll(x => x.VO_NAME.ToUpper().Contains(txt.Text.ToString().ToUpper())).OrderBy(z => z.VO_NAME).ToList(); }
+                    //else
+                    //{filteredlist = Sourcerecords.FindAll(x => x.VO_NAME.ToUpper().Contains(txt.Text.ToString().ToUpper())).OrderBy(z => z.VO_NAME).ToList(); }
+
+                    List<VOListing> filteredlist = (from val in Sourcerecords
+                                                    where val.VO_NAME.ToUpper().Contains((txt.Text == "" ? val.VO_NAME.ToUpper() : txt.Text.ToUpper()))
+                                                    && val.IntervalNumber.ToUpper().Contains(txtInterval.Text == "" ? val.IntervalNumber.ToUpper() : txtInterval.Text.ToUpper())
+                                                    && val.OwnerNumber.ToUpper().Contains(txtOwnerNumber.Text == "" ? val.OwnerNumber.ToUpper() : txtOwnerNumber.Text.ToUpper())
+                                                    select val).ToList();
+
+
+                    dataGridView1.DataSource = filteredlist;
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            TextBox txt = sender as TextBox;
+
+            if (txt.Name == txtOwnerNumber.Name)
+            {
+                if (txt.Text == "")
+                {
+                    dataGridView1.DataSource = Sourcerecords;
+                }
+                else
+                {
+                    //List<VOListing> filteredlist;
+                    if (txtInterval.Text != "" || txtName.Text != "")
+                    {filteredlist = filteredlist.FindAll(x => x.OwnerNumber.ToUpper().Equals(txt.Text.ToString().ToUpper())).OrderByDescending(z => z.OwnerNumber).ToList(); }
+                    else
+                    {filteredlist = Sourcerecords.FindAll(x => x.OwnerNumber.ToUpper().Equals(txt.Text.ToString().ToUpper())).OrderByDescending(z => z.OwnerNumber).ToList(); }
+                        
+                    dataGridView1.DataSource = filteredlist;
+                }
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            TextBox txt = sender as TextBox;
+
+            if (txt.Name == txtInterval.Name)
+            {
+                if (txt.Text == "")
+                {
+                    dataGridView1.DataSource = Sourcerecords;
+                }
+                else
+                {
+                    //List<VOListing> filteredlist;
+                    if (txtOwnerNumber.Text != "" || txtName.Text != "")
+                    {filteredlist = filteredlist.FindAll(x => x.IntervalNumber.ToUpper().Equals(txt.Text.ToString().ToUpper())).OrderBy(z => z.IntervalNumber).ToList();}
+                    else
+                    {filteredlist = Sourcerecords.FindAll(x => x.IntervalNumber.ToUpper().Equals(txt.Text.ToString().ToUpper())).OrderBy(z => z.IntervalNumber).ToList(); }                        
+                    dataGridView1.DataSource = filteredlist;
+                }
+            }
+        }
+
+
+        private void textbox_TextChanged(object sender, EventArgs e)
+        {
+            TextBox txt = sender as TextBox;
+
+
+            //if (txt.Name == txtName.Name)
+            //{
+                //if (txt.Text == "")
+                //{
+                //    dataGridView1.DataSource = Sourcerecords;
+                //}
+                //else
+                //{
+                    //List<VOListing> filteredlist;
+
+                    //if (txtInterval.Text != "" || txtOwnerNumber.Text != "")
+                    //{filteredlist = filteredlist.FindAll(x => x.VO_NAME.ToUpper().Contains(txt.Text.ToString().ToUpper())).OrderBy(z => z.VO_NAME).ToList(); }
+                    //else
+                    //{filteredlist = Sourcerecords.FindAll(x => x.VO_NAME.ToUpper().Contains(txt.Text.ToString().ToUpper())).OrderBy(z => z.VO_NAME).ToList(); }
+
+                    List<VOListing> filteredlist = (from val in Sourcerecords
+                                                    where val.VO_NAME.ToUpper().Contains((txtName.Text == "" ? val.VO_NAME.ToUpper() : txtName.Text.ToUpper()))
+                                                    && val.IntervalNumber.ToUpper().Equals(txtInterval.Text == "" ? val.IntervalNumber.ToUpper() : txtInterval.Text.ToUpper())
+                                                    && val.OwnerNumber.ToUpper().Contains(txtOwnerNumber.Text == "" ? val.OwnerNumber.ToUpper() : txtOwnerNumber.Text.ToUpper())
+                                                    select val).ToList();
+
+
+                    dataGridView1.DataSource = filteredlist;
+                }
+
+        private void txtButton_Click(object sender, EventArgs e)
+        {
+            txtInterval.Text = "";
+            txtName.Text = "";
+            txtOwnerNumber.Text = "";
+            cbInterval.SelectedIndex = 0;
+        }
+
+        private void cbInterval_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtInterval.Text = cbInterval.SelectedValue.ToString();
+        }
+        //}
+        //}
+
+    }
+    class VOListing
+    {
+
+        public string VO_NAME {get;set;}
+        public string OwnerNumber { get; set; }
+        public string UnitNumber { get; set; }
+        public string IntervalNumber { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Country { get; set; }
+        public string Phone1 { get; set; }
+        public string Phone2 { get; set; }
+        public string Email { get; set; }
+        public string CUorVO { get; set; }
+        public string EF_or_1B {get;set;}
+        public string BalanceOwing { get; set; }
+}
+}
